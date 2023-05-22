ea5b80a8b80629438b170994d7c387df05ee9580
Gerardo <geerardoceeronmeena@gmail.com>
2023-05-21 21:52:09 -0400

    Agregando desarrollo

--- Lab2/Eje2.dart
+++ Lab2/Eje2.dart
@@ -1,3 +1,19 @@
+import 'dart:math';
+
 void main() {
+  // creamos la primera lista con los elementos que se entregaron
   List<int> lista = [3, 4, 7, 9, 8, 5, 1, 2, 5, 7];
+
+  // creamos la segunda lista con elementos randoms entre -1 y -5
+  List<int> lista2 = List.generate(10, (index) => Random().nextInt(5) - 5);
+  //
+  List<int> lista3 = [];
+  for (int i = 0; i < lista.length; i++) {
+    lista3.add(lista[i] + lista2[i]);
+  }
+  print(lista3);
+
+  lista3.removeAt(7);
+  lista3.removeAt(7);
+  print(lista3);
 }
